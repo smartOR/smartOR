80d9a0554656e4666d5ad21c5fa78dd4d4039198
Muhammed-Ali Findik <muhammed-ali.findik@richard-wolf.com>
2013-08-02 19:16:51 +0200

    Initial import into github Repo

--- AccessControlManager/src/main/java/de/smartor/manager/accesscontrolmanager/IdentificationService.java
+++ AccessControlManager/src/main/java/de/smartor/manager/accesscontrolmanager/IdentificationService.java
@@ -1,3 +1,23 @@
+/********************************************************************************
+ * Copyright (c) 2013 Muhammed-Ali Findik					*
+ * "Richard Wolf Gmbh" spirit of excellence [http://www.richard-wolf.com]	*
+ *										*
+ * This file is part of smartOR AccessControlManager.				*
+ * 										*
+ * smartOR AccessControlManager is free software: you can redistribute 		*
+ * it and/or modify it under the terms of the GNU General Public License 	*
+ * as published by the Free Software Foundation, either version 3 of the	*
+ * License, or (at your option) any later version.				*
+ * 										*
+ * This program is distributed in the hope that it will be useful,		*
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of		*
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		*
+ * GNU General Public License for more details.					*
+ * 										*
+ * You should have received a copy of the GNU General Public License		*
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.	*
+ ********************************************************************************/
+
 package de.smartor.manager.accesscontrolmanager;
 
 import java.io.IOException;
